0b90918ca49d1e6ae4b6851b929e73eb5f8c876a
st <st@nuc11.lan>
2024-09-09 03:17:02 -0400

    Feat: String

--- lua4tinker.h
+++ lua4tinker.h
@@ -7,6 +7,11 @@ extern "C" {
     #include "lauxlib.h"
 }
 
+#include <optional>
+#include <cassert>
+#include <concepts>
+#include <typeinfo>
+
 namespace lua4tinker {
 
     lua_State *new_state() {
@@ -25,34 +30,82 @@ namespace lua4tinker {
         return lua_dostring(L, code.c_str());
     }
 
+    struct stack_delay_pop {
+        lua_State *L;
+        int pop_num;
+        stack_delay_pop(lua_State *_L, int _pop_num) : L(_L), pop_num(_pop_num) {}
+        ~stack_delay_pop() { lua_pop(L, pop_num); }
+    };
+
     // Helper Function End
 
     template <typename T>
     struct stack_helper {
         static T read(lua_State *L) {
-            T v;
-            return v;
+            return std::make_optional<T>;
         }
-        static void push(lua_State *L, T && val) {}
+        static void push(lua_State *L, T && val) {
+            std::cout << typeid(val).name() << std::endl;
+            assert(!"尚未处理的push");
+        }
     };
 
     template <>
-    struct stack_helper<int> {
-        static int read(lua_State *L) { return 0; }
-        static void push(lua_State *L, int val) {
-            lua_pushnumber(L, val);
+    struct stack_helper<std::string> {
+        static void push(lua_State *L, const std::string & val) {
+            lua_pushlstring(L, val.c_str(), val.size());
+        }
+
+        static std::string read(lua_State *L) {
+            if (lua_type(L, 1) == LUA_TSTRING) {
+                const char *str = lua_tostring(L, 1);
+                size_t str_size = lua_strlen(L, 1);
+                return std::string(str, str_size);
+            }
+            return "";
         }
     };
 
     template <typename T>
-    void push(lua_State *L, T && val) {
-        stack_helper<T>::push(L, std::forward<T>(val));
-    }
+    concept LuaNumber = std::is_integral_v<T> || std::is_floating_point_v<T>;
+    // 试试使用requires语法
+    // requires std::integral<T> || std::floating_point<T>
+    template <LuaNumber T>
+    struct stack_helper<T> {
+        static void push(lua_State *L, T val) {
+            lua_pushnumber(L, val);
+        }
+        static T read(lua_State *L) {
+            if (lua_type(L, 1) == LUA_TNUMBER) {    // lua4中number包含数值和字符串型的数值
+                return (T)lua_tonumber(L, 1);
+            } else {
+                assert(!"堆栈中不是number");
+            } 
+            return 0; 
+        }
+    };
+
+    // template <typename T>
+    // void push(lua_State *L, T && val) {
+    //     stack_helper<T>::push(L, std::forward<T>(val));
+    // }
+
+    // template <typename T>
+    // T read(lua_State *L) {
+    //     return stack_helper<T>::read(L);
+    // }
 
     template <typename T>
     void set(lua_State* L, const char* name, T&& object) {
-        push(L, std::forward<T>(object));
+        stack_helper<std::decay_t<T>>::push(L, std::forward<T>(object));
         lua_setglobal(L, name);
+    }
+
+    template <typename T>
+    T get(lua_State *L, const char* name) {
+        lua_getglobal(L, name);
+        stack_delay_pop tmp(L, 1);
+        return stack_helper<T>::read(L);
     }
 }
 
--- test/luatest_setglobalval.cpp
+++ test/luatest_setglobalval.cpp
@@ -5,11 +5,39 @@ int main() {
     lua_State * L = lua4tinker::new_state();
     lua4tinker::open_libs(L);
 
-    lua4tinker::set(L, "test_str", 1);
+    int int_val = 233;
+    float float_val = 233.233;
+    std::string string_val = "Hello, this is lua4tinker!";
+
+    lua4tinker::set(L, "int_val", int_val);
+    lua4tinker::set(L, "float_val", float_val);
+    lua4tinker::set(L, "string_val", string_val);
     
     std::string code = R"(
-        print(test_str)
+        print(int_val)
+        print(float_val)
+        print(string_val)
+        int_val2 = int_val
+        float_val2 = float_val
+        string_val2 = string_val
     )";
 
-    return lua4tinker::do_string(L, code);
+    int ret = lua4tinker::do_string(L, code);
+
+    if (!ret) {
+        int int_val2 = lua4tinker::get<int>(L, "int_val2");
+        float float_val2 = lua4tinker::get<float>(L, "float_val2");
+        std::string string_val2 = lua4tinker::get<std::string>(L, "string_val2");
+
+        if (
+                int_val2 != int_val ||
+                float_val2 != float_val ||
+                string_val2 != string_val
+            ) 
+        {
+                return 1;
+        }
+
+    }
+    return ret;
 }
