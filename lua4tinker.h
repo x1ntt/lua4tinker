831f0c1eed33d1eaf1210671514a2f3e77fa5166
x1nt <cjdty@qq.com>
2024-12-03 03:38:26 -0500

    Feat: 实现对象传递🎉

--- lua4tinker.h
+++ lua4tinker.h
@@ -160,7 +160,7 @@ namespace lua4tinker {
     }
 
     template <typename T>
-    T read(lua_State *L, size_t index) {
+    T read(lua_State *L, int index) {
         return stack_helper<std::decay_t<T>>::read(L, index);
     }
 
@@ -178,6 +178,21 @@ namespace lua4tinker {
     {
         return direct_invoke_invoke_helper<nIdxParams, RVal, Func, Args...>(std::forward<Func>(func),
             L,
+            std::make_index_sequence<sizeof...(Args)>{});
+    }
+
+    template<int32_t nIdxParams, typename RET_T, typename Func, typename CLASS_T, typename... Args, std::size_t... index>
+    RET_T direct_invoke_invoke_helper(Func&& func, lua_State* L, CLASS_T* object_ptr, std::index_sequence<index...>)
+    {
+        return func(object_ptr, read<Args>(L, index + nIdxParams)...);
+    }
+
+    template<int32_t nIdxParams, typename RET_T, typename Func, typename CLASS_T, typename... Args>
+    RET_T direct_invoke_member_func(Func&& func, lua_State* L, CLASS_T* object_ptr)
+    {
+        return direct_invoke_invoke_helper<nIdxParams, RET_T, Func, CLASS_T, Args...>(std::forward<Func>(func),
+            L,
+            object_ptr,
             std::make_index_sequence<sizeof...(Args)>{});
     }
 
@@ -227,6 +242,28 @@ namespace lua4tinker {
             std::cout << "gc 调用析构函数" << std::endl;
             return 0;
         }
+    };
+
+    template <typename CLASS_T, typename RET_T, typename... Args>
+    struct mem_func : public functor_base {
+        using FuncWarpType = mem_func<CLASS_T, RET_T, Args...>;
+        typedef std::function<RET_T(CLASS_T*, Args...)> FunctionType;
+
+        FunctionType _func;
+        std::string _mem_func_name;
+        CLASS_T *_object_ptr;
+        mem_func (FunctionType && func, const char *mem_func_name, CLASS_T *object_ptr) : _func(std::move(func)), _mem_func_name(mem_func_name), _object_ptr(object_ptr) { }
+        virtual int32_t apply(lua_State* L) {
+            if constexpr (std::is_void_v<RET_T>) {  // 无返回值
+                direct_invoke_member_func<2, RET_T, FunctionType, CLASS_T, Args...>(std::forward<FunctionType>(_func), L, _object_ptr);
+                return 0;
+            } else {    // 有返回值
+                push<RET_T>(L, direct_invoke_member_func<2, RET_T, FunctionType, CLASS_T, Args...>(std::forward<FunctionType>(_func), L, _object_ptr));
+                return 1;
+            }
+        };
+
+
     };
 
     template <typename R, typename... Args>
@@ -303,10 +340,6 @@ namespace lua4tinker {
         };
     };
 
-    struct base_func {
-        int32_t apply(lua_State* L) {};
-    };
-
     int mem_set_tag_func(lua_State *L) {
         // cout << "mem_set_tag_func" << endl;
         // enum_stack(L);
@@ -324,10 +357,9 @@ namespace lua4tinker {
 
     int mem_get_tag_func(lua_State *L) {
         // cout << "mem_get_tag_func" << endl;
-        enum_stack(L);
+        // enum_stack(L);
         lua_pushvalue(L, 2);
         lua_rawget(L, 1);
-        enum_stack(L);
         auto *mem_var_ptr = (base_var*)lua_touserdata(L, -1);
         if (mem_var_ptr) {
             mem_var_ptr->get(L);    // push 结果到栈上去
@@ -341,7 +373,18 @@ namespace lua4tinker {
 
     int mem_function_call_func(lua_State *L) {
         cerr << "mem_function_call_func " << endl;
-        enum_stack(L);
+        /*
+            index(3, LUA_TUSERDATA) = 0x448508
+            index(2, LUA_TNUMBER) = 123
+            index(1, LUA_TTABLE)
+        */
+        // enum_stack(L);
+
+        if (lua_type(L, lua_upvalueindex(1, 1) == LUA_TUSERDATA)) {
+            auto *mem_func = (functor_base*)lua_touserdata(L, lua_upvalueindex(1, 1));
+            lua_remove(L, lua_upvalueindex(1, 1));  // 移除upvalue
+            return mem_func->apply(L);
+        }
         return 0;
     }
     
@@ -412,15 +455,16 @@ namespace lua4tinker {
         }
     }
 
-    template <typename BASE, typename FUNC>
-    void class_object_func(lua_State *L, BASE *object_ptr, const char *object_name, FUNC && mem_func_ptr, const char *mem_func_name) {
+    template <typename CLASS_T, typename RET_T, typename... ARGS>
+    void class_object_func(lua_State *L, CLASS_T *object_ptr, const char *object_name, RET_T (CLASS_T::*func)(ARGS...), const char *mem_func_name) {
         stack_scope_exit scope_exit(L);
         lua_getglobal(L, object_name);
         if (lua_type(L, 1) == LUA_TTABLE) {
             lua_pushstring(L, mem_func_name);
-            new (lua_newuserdata(L, sizeof(base_func))) base_func();
-            lua_pushcfunction(L, mem_function_call_func);
-            enum_stack(L);
+            using FuncWarpType = mem_func<CLASS_T, RET_T, ARGS...>;
+            new (lua_newuserdata(L, sizeof(FuncWarpType))) FuncWarpType(func, mem_func_name, object_ptr);
+            lua_pushcclosure(L, mem_function_call_func, 1);
+            // enum_stack(L);
             lua_rawset(L, -3);
         }else {
             assert(!"需要事先通过class_object定义对象");
--- test/luatest_object.cpp
+++ test/luatest_object.cpp
@@ -36,9 +36,9 @@ int main() {
             print (a_object['_a'])
             print (a_object['_str'])
             print (a_object['_flt'])
-            a_object:sum(int_val)
+            print ("a_object:sum: "..a_object:sum(int_val, float_val))
             -- print (a_object(int_val))
-            return 123
+            return a_object['_a']+a_object['_a']
         end
     )");
 
@@ -50,23 +50,9 @@ int main() {
     lua4tinker::class_object_func<A>(L, &a_object, "a_object", &A::sum, "sum");
 
     int int_val = 123;
-    if (lua4tinker::call<int>(L, "call_by_cpp", lua4tinker::LuaClass("a_object"), int_val, string("Hello, Object!"), 233.233) != int_val) {
+    if (lua4tinker::call<int>(L, "call_by_cpp", lua4tinker::LuaClass("a_object"), int_val, string("Hello, Object!"), 233.233) != int_val*2) {
         return 1;
     }
     a_object.dump();
     return 0;
 }
-
-/*
-# 传递对象
-    + class_object(L, "class_obj", &class_obj);  // 创建table
-    + class_object_mem(L, &class_obj, &class_obj.a1); // 填充table的key，
-
-    首先 class_object_def() 创建一个table以对象为名，设定好index和settable，然后class_object_mem_def() 填充table的key
-        class_object_mem_def() 将成员指针和对象指针放入一个mem_warp中在newuserdata中初始化，并且将userdata放入table中
-    当Lua侧访问这个成员时，会触发到index方法，此时该方法中可以通过栈获取到table和成员名，根据这个获取到mem_warp，
-    将对象指针和成员指针取出来，然后调用对应mem_warp的get方法，获取到成员值
-
-    lua4tinker::call() 的时候，参数应该是上面定义的table，push中的实际逻辑应该是根据对象名把对应table push到栈顶。
-    具体实现，通过类LuaClass实现模板特化，stack_helper中实现lua_getglobal()
-*/
